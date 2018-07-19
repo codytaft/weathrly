9a72d3025abb6338ed9c02fd757abd696eabbd75
Cody Taft <cody.taft@gmail.com>
2018-07-19 15:06:23 -0600

    Begin organizing for API call

--- src/10DayForecast.js
+++ src/10DayForecast.js
@@ -1,3 +1,11 @@
+// tenDayForecast: [ {
+//   date: '',   // forecast.simpleforecast.forecastday[i].date.pretty
+//   icon: '',   // forecast.simpleforecast.forecastday[i].icon_url
+//   hi: 0,      // forecast.simpleforecast.forecastday[i].high.fahrenheit
+//   lo: 0,      // forecast.simpleforecast.forecastday[i].low.fahrenheit
+// } ]
+
+
 // Import card
 // Import component
 // create class extends component
