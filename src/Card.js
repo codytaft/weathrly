e0ff3dc642ae446fb3e9e3203ef4f74c9f4d4f44
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-07-21 16:45:19 -0600

    Create method to make card

--- src/Card.js
+++ src/Card.js
@@ -1,34 +1,31 @@
-import getAllWeatherData from './weather.js' 
-
-import React, { Component } from 'react';
-
-class Card extends Component {
-  constructor() {
-    super();
-    this.state = {
-      currentHour: '',
-      currentDay: '',
-      currentIcon: '',
-      projectedTemp: '',
-      projectedHi: '',
-      projectedLo: ''
-    }
-  }
-
-  setCurrentHour
-
-  render() {
-    return(
-      <div>
-        <section>
-          <p className="hour-day"></p>
-          <img className="hour-day-icon" />
-          <p className="projected-temp"></p>
-        </section>
-      </div>
-        
-    )
-  }
+const getWeatherCard = (icon, time, temp, loTemp = '') => {
+  return (
+      <section>
+        <img className="card_icon" src={icon}/>
+        <p className="card_temp">{temp}<span className="card_temp--lo">{loTemp}</span></p>
+        <p className="card_time">{time}</p>
+      </section>  
+  )
 }
 
-export default Card
+// import App from './App.js' 
+
+// import React, { Component } from 'react';
+
+// class Card extends Component {
+//   constructor() {
+//     super();
+//   }
+
+//   render() {
+//     return(
+//       <section>
+//         <img className="card_icon" src={}/>
+//         <p className="card_temp"><span className="card_temp--lo"></span></p>
+//         <p className="card_time"></p>
+//       </section>  
+//     )
+//   }
+// }
+
+export default Card;
