c5c3a937152d8cac547542291e6808105b7f0990
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-07-22 11:02:42 -0600

    Style current weather and add normalize.css

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -9,13 +9,13 @@ class CurrentWeather extends Component {
 
   render() {
     return(
-      <section className="current-weather">
+      <section className="Results__current-weather">
         <img src={this.props.locationData.currentWeather.currentTempIcon}/>
-        <p className="card_temp">{this.props.locationData.currentWeather.currentHi}<span className="card_temp--lo">{this.props.locationData.currentWeather.currentLo}</span></p>
-        <p className="current-weather">{this.props.locationData.currentWeather.currentTemp}&#8457;</p>
-        <p>{this.props.locationData.currentWeather.currentDescription}</p>
-        <p>TODAY</p>
-        <p>{this.props.locationData.currentWeather.currentDay} {this.props.locationData.currentWeather.currentHour}:00</p>
+        <p className="Results__current-weather--temp">{this.props.locationData.currentWeather.currentTemp}&#8457;</p>
+        <p className="card__temp">{this.props.locationData.currentWeather.currentHi}<span className="card__temp--lo">{this.props.locationData.currentWeather.currentLo}</span></p>
+        <p className="Results__current-weather--description">{this.props.locationData.currentWeather.currentDescription}</p>
+        <p className="Results__current-weather--today" >TODAY</p>
+        <p className="Results__current-weather--date">{this.props.locationData.currentWeather.currentDay} {this.props.locationData.currentWeather.currentHour}:00</p>
       </section>
     ) 
   }
