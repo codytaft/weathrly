bf223d17f6ca8459d50d81c36e94b42d32cc247f
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-07-22 14:31:36 -0600

    Style current weather

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -11,7 +11,7 @@ class CurrentWeather extends Component {
     return(
       <section className="Results__current-weather">
         <img src={this.props.locationData.currentWeather.currentTempIcon}/>
-        <p className="Results__current-weather--temp">{this.props.locationData.currentWeather.currentTemp}&#8457;</p>
+        <p className="Results__current-weather--temp">{this.props.locationData.currentWeather.currentTemp}<span className="Results__current-weather--temp-degree"> &#176;F</span></p>
         <p className="card__temp">{this.props.locationData.currentWeather.currentHi}<span className="card__temp--lo">{this.props.locationData.currentWeather.currentLo}</span></p>
         <p className="Results__current-weather--description">{this.props.locationData.currentWeather.currentDescription}</p>
         <p className="Results__current-weather--today" >TODAY</p>
