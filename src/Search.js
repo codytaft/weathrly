fb432e7e083255a8892f3c22b3b976c31bd0ba19
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-07-23 19:31:56 -0600

    Create datalist to display suggestions

--- src/Search.js
+++ src/Search.js
@@ -10,27 +10,43 @@ class Search extends Component {
   constructor() {
     super();
     this.state = {
-      inputVal: ''
+      inputVal: '',
+      suggestions: []
     }
   }
 
   updateVal = (e) => {
     this.setState({ inputVal: e.target.value });
-    this.getSuggestions(e.target.value);
-  }
-
-  getSuggestions = (prefix) => {
-    console.log(prefix)
-    const suggestions = citiesTrie.suggest(prefix);
-    console.log(suggestions);
+    if (e.target.value.length > 1) {
+      this.setState({ 
+        suggestions: citiesTrie.suggest(e.target.value)
+      });
+    } else {
+       this.setState({ suggestions: [] });
+    }
   }
 
   render() {
     return(
-      <form className="Search">
-        <input className="Search__input" type="text" placeholder="ENTER CITY/STATE OR ZIP CODE" onChange={this.updateVal}/>
-        <button className="Search__button" onClick={() => this.props.setLocation(this.state.inputVal)}></button> 
-      </form>
+        <form className="Search">
+          <input className="Search__input" list="suggested-cities" type="text" placeholder="ENTER CITY/STATE OR ZIP CODE" onChange={this.updateVal}/>
+           <datalist id="suggested-cities">
+            { this.state.suggestions.map( (suggestion, i) => {
+                return (
+                  <option key={i}
+                          value={suggestion}
+                          onClick={() => {
+                          citiesTrie.select(suggestion);
+                          this.setState({ inputVal: suggestion, 
+                                          suggestedCities: []
+                                        });
+                  }} />
+                );
+              })
+            }
+          </datalist>
+          <button className="Search__button" onClick={() => this.props.setLocation(this.state.inputVal)}></button> 
+        </form>
     )
   }
 }
