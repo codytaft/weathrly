628f8044e4ec1902bd7a8ffdb7c9c8f8b2b95e47
Cody Taft <cody.taft@gmail.com>
2018-07-24 16:28:47 -0600

    Finish weather.js testing

--- src/weather.test.js
+++ src/weather.test.js
@@ -20,4 +20,69 @@ describe('weather', () => {
 
   })
 
+  it('should return hourlyWeather', () => {
+    const cleanData = getAllWeatherData(data);
+    expect(cleanData.hourlyWeather).toEqual([{ 
+                                                hour: '13',
+                                                icon: 'http://icons.wxug.com/i/c/k/partlycloudy.gif',
+                                                temp: '49' },
+                                              { hour: '14',
+                                                icon: 'http://icons.wxug.com/i/c/k/partlycloudy.gif',
+                                                temp: '49' },
+                                              { hour: '15',
+                                                icon: 'http://icons.wxug.com/i/c/k/clear.gif',
+                                                temp: '51' },
+                                              { hour: '16',
+                                                icon: 'http://icons.wxug.com/i/c/k/clear.gif',
+                                                temp: '50' },
+                                              { hour: '17',
+                                                icon: 'http://icons.wxug.com/i/c/k/clear.gif',
+                                                temp: '48' },
+                                              { hour: '18',
+                                                icon: 'http://icons.wxug.com/i/c/k/nt_clear.gif',
+                                                temp: '45' },
+                                              { hour: '19',
+                                                icon: 'http://icons.wxug.com/i/c/k/nt_clear.gif',
+                                                temp: '43' }])
+  })
+
+  it('should return dailyWeather', () => {
+    const cleanData = getAllWeatherData(data);
+    expect(cleanData.dailyWeather).toEqual([ { day: 'Thursday',
+                                                icon: 'http://icons.wxug.com/i/c/k/clear.gif',
+                                                high: '55',
+                                                low: '51' },
+                                              { day: 'Friday',
+                                                icon: 'http://icons.wxug.com/i/c/k/chancerain.gif',
+                                                high: '57',
+                                                low: '44' },
+                                              { day: 'Saturday',
+                                                icon: 'http://icons.wxug.com/i/c/k/rain.gif',
+                                                high: '47',
+                                                low: '30' },
+                                              { day: 'Sunday',
+                                                icon: 'http://icons.wxug.com/i/c/k/cloudy.gif',
+                                                high: '37',
+                                                low: '22' },
+                                              { day: 'Monday',
+                                                icon: 'http://icons.wxug.com/i/c/k/clear.gif',
+                                                high: '35',
+                                                low: '19' },
+                                              { day: 'Tuesday',
+                                                icon: 'http://icons.wxug.com/i/c/k/partlycloudy.gif',
+                                                high: '32',
+                                                low: '20' },
+                                              { day: 'Wednesday',
+                                                icon: 'http://icons.wxug.com/i/c/k/partlycloudy.gif',
+                                                high: '33',
+                                                low: '26' },
+                                              { day: 'Thursday',
+                                                icon: 'http://icons.wxug.com/i/c/k/snow.gif',
+                                                high: '35',
+                                                low: '23' },
+                                              { day: 'Friday',
+                                                icon: 'http://icons.wxug.com/i/c/k/partlycloudy.gif',
+                                                high: '31',
+                                                low: '18' } ])
+                                              })
 })
