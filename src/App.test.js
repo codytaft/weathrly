29e0a64e19e47e2cb764b763b1c33585cadfdfe5
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-07-25 17:37:08 -0600

    Add tests to App

--- src/App.test.js
+++ src/App.test.js
@@ -12,38 +12,77 @@ describe('App component', () => {
     wrapper = shallow(<App />);
   });
 
-  // it('renders without crashing', () => {
-  //   const div = document.createElement('div');
-  //   ReactDOM.render(<App />, div);
-  //   ReactDOM.unmountComponentAtNode(div);
-  // });
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 
   it('should exist', () => {
-    console.log(localStorage.getItem('savedLocation'))
     expect(wrapper).toBeDefined();
   });
 
-  it.skip('should have a default state with an empty object of locationData and empty string of searchedLocation', () => {
+  it('should have a default state with an empty object of locationData and empty string of searchedLocation', () => {
     expect(wrapper.state()).toEqual({locationData: {}, 
-                                   searchedLocation: ''})
+                                   searchedLocation: '',
+                                   dataLoaded: false,
+                                   error: false})
   });
 
-  it.skip('should render a welcome page', () => {
-
+  it('should render a welcome page with a welcome and search component', () => {
+    expect(wrapper.find('Welcome').length).toEqual(1);
+    expect(wrapper.find('Search').length).toEqual(1);
   });
 
-  it.skip('should render an error page if there is a catch error', () => {
+  it.skip('should render an error page with an error and search component if there is a catch error', () => {
+    wrapper.state().error = true;
+    // wrapper.instance().render();
 
+    expect(wrapper.find('Error').length).toEqual(1);
+    expect(wrapper.find('Search').length).toEqual(1);
   });
 
-  it.skip('should render a results page with weather data', () => {
+  it.skip('should render a results page with current weather, hourly forecast, and daily forecast components', () => {
+    wrapper.state().dataLoaded = true;
+    wrapper.state().searchedLocation = 'Denver, CO';
 
+    expect(wrapper.find('CurrentWeather').length).toEqual(1);
+    expect(wrapper.find('HourlyForecast').length).toEqual(1);
+    expect(wrapper.find('DailyForecast').length).toEqual(1);
   });
 
-  it.skip('should save city ', () => {
+  it.skip('should update state, run a function that fetches API, and save location to localStorage', () => {
+    const searchedLocation = 'Denver, CO';
+    
+    expect(wrapper.state().searchedLocation).toEqual('');
 
+    wrapper.instance().setLocation(searchedLocation);
+
+    expect(wrapper.state().searchedLocation).toEqual('Denver, CO');
   });
 
+  it.skip('when component loads it should check if there is a city in locale storage and set error to false', () => {
+    expect(wrapper.state().error).toEqual(false);
+    expect(wrapper.state().searchedLocation).toEqual('');
+
+    localStorage.setItem('savedLocation', 'Denver, CO');
+    wrapper = mount(<App />);
+
+    expect(wrapper.state().searchedLocation).toEqual('Denver, CO');
+  });
+
+  it('when component loads it should have a state error property of false', () => {
+    expect(wrapper.state().error).toEqual(false);
+  });
+
+  it.skip('should return to welcome page when home button is clicked', () => {
+    const homeButton = wrapper.find('.Home-button');
+
+    homeButton.simulate('click');
+
+    expect(wrapper.find('Welcome').length).toEqual(1);
+    expect(wrapper.find('Search').length).toEqual(1);
+  });
   
 })
 
