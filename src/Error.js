3518ad53abe2e3bc29fca89cbdfaea4db36b0c5e
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-07-24 21:15:54 -0600

    Add space in header

--- src/Error.js
+++ src/Error.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Error = (props) => {
   return(
     <div >
-      <h1 className="error-msg">Uh Oh..Does your city exist?</h1>
+      <h1 className="error-msg">Uh Oh... Does your city exist?</h1>
       <h3 className="error-hint">make sure to use City, State format</h3>
     </div>
   )
