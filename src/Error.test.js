078f23b8262a31f5fb353f054e16d01550384080
Cody Taft <cody.taft@gmail.com>
2018-07-25 19:05:26 -0600

    Finish error testing

---
+++ src/Error.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
+
+import Error from './Error';
+
+describe('Error', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Error />);
+  })
+
+  it('should exist', () => {
+    expect(wrapper).toBeDefined();
+  })
+
+  it('should show header with error message', () => {
+    const header = wrapper.find('.error-msg');
+
+    expect(header).toBeDefined();
+    expect(header.text()).toEqual('Uh Oh... Does your city exist?')
+  })
+
+  it('should show smaller header with error hint', () => {
+    const header = wrapper.find('.error-hint');
+
+    expect(header).toBeDefined();
+    expect(header.text()).toEqual('make sure to use City, State format')
+  })
+})
