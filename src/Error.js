d2877e8cb441c5645e8a1a6108a225d0efc1be7d
Cody Taft <cody.taft@gmail.com>
2018-07-26 21:38:06 -0600

    Run linter on Error.js

--- src/Error.js
+++ src/Error.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const Error = () => {
-  return(
-    <div >
+  return (
+    <div>
       <h1 className="error-msg">Uh Oh... Does your city exist?</h1>
       <h3 className="error-hint">make sure to use City, State format</h3>
     </div>
-  )
-}
+  );
+};
 
 export default Error;
