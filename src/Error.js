f3d8ff3ff7edf19559764f119e27b09ca02f181f
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-07-26 22:54:06 -0600

    Rename classes using BEM

--- src/Error.js
+++ src/Error.js
@@ -3,8 +3,8 @@ import React from "react";
 const Error = () => {
   return (
     <div>
-      <h1 className="error-msg">Uh Oh... Does your city exist?</h1>
-      <h3 className="error-hint">make sure to use City, State format</h3>
+      <h1 className="Error__msg">Uh Oh... Does your city exist?</h1>
+      <h3 className="Error__msg-hint">make sure to use City, State format</h3>
     </div>
   );
 };
